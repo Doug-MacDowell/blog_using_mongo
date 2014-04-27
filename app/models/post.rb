a43fd38afc45ee465c6db6d80b9c71fbfaf126f0
Doug MacDowell <dougmac9@uw.edu>
2014-04-27 15:34:17 -0700

    added parameterization for title string

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,18 @@
-Class Post
+class Post
 
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Mongoid::Paranoia
 
-  field   :_id, type: String, default: ->{ name.to_s.parameterize }
+# The Paranoia framework throws errors, disabled it
+# include Mongoid::Paranoia
+
+  field   :_id, type: String, default: -> { title.to_s.parameterize }
   field :title, type: String
   field  :body, type: String
 
   validates :title, presence: true
   validates :title, uniqueness: { conditions: -> { where(deleted_at: nil) } }
 
-  embeds_many :comments
+ # embeds_many :comments
 
 end
