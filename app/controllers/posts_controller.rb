eaab868fbede22b4c933020ed1deb5a78dc5f733
Doug MacDowell <dougmac9@uw.edu>
2014-04-27 14:05:36 -0700

    added index, new, and create methods

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts =Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Your Post has been added to our blog!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Could not add this Post"
+      render "new"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
