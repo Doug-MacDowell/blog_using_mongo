2c64eab2ab86fe7a27be0a44168c604b86533d47
Doug MacDowell <dougmac9@uw.edu>
2014-04-27 15:33:03 -0700

    added more methods to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts =Post.all
+  end
+
+  def show
   end
 
   def new
     @post = Post.new
   end
 
+  def edit
+  end
+
   def create
     @post = Post.new(post_params)
-    if @post.save
-      flash[:success] = "Your Post has been added to our blog!"
-      redirect_to posts_path
-    else
-      flash[:error] = "Could not add this Post"
-      render "new"
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Your Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:title, :body)
   end
 
 end
